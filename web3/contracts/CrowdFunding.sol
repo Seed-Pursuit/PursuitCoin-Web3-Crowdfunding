51444d67850e0a813038fe8b5596866868995a84
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-08-22 18:06:58 +0530

    updated

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -34,13 +34,17 @@ contract CrowdFunding {
         noOfCampaigns++;
         return noOfCampaigns-1;
     }
-
+ 
     function donateToCamapaign(uint256 _id) public payable {
         uint256 amount = msg.value;
+        
         Campaign storage campaign = campaigns[_id];
+        
         campaign.donators.push(msg.sender);
         campaign.donations.push(amount);
+        
         (bool sent,) = payable(campaign.owner).call{value: amount}("");
+        
         if(sent){
             campaign.amountCollected = campaign.amountCollected + amount;
         }
